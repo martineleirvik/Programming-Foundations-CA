761fa31fd829a6eef4c5ae057c33de3c2786cd28
martineleirvik <leirvikmartine@gmail.com>
2021-01-16 18:55:05 +0100

    little more

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+
 // question 1
 
 var outOfStock;
@@ -11,5 +12,70 @@ if (outOfStock === true) {
 
 console.log(outOfStock);
 
+
 //question 2
+
+for (var i = 15; i<=25; i++) {
+    if (i === 17, i===20) {
+    console.log(i);
+    }
+}
+
+
+//question 3
+
+var games = [
+	{
+		title: "Grand Theft Auto",
+		rating: 4.48,
+	},
+	{
+		title: "Portal 2",
+		rating: 3.5,
+	},
+	{
+		title: "Team Fortress",
+		rating: null,
+	},
+	{
+		title: "The Witcher",
+		rating: 3.0,
+	},
+	{
+		title: "The Elder Scrolls",
+		rating: 2.9,
+	},
+];
+
+
+for (var i = 0; i < games.length; i++) {
+    console.log(games[i]);
+}
+
+
+//question 4
+
+var whatIDontLike = "bugs";
+
+if (typeof val === "string") {
+    console.log("Variable is a string");
+} else {
+    console.log("Variable is not a string");
+}
+
+
+function whatIDontLike(bugs) {
+    return typeof value === "string" 
+}
+
+
+// question 5
+
+function logTwoArguments(yes, no) {
+    console.log(yes + no);
+}
+
+logTwoArguments(2, 8);
+
+
 
