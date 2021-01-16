c55e9c9ddeace0a375c1419c7766bbc2c47f3926
martineleirvik <leirvikmartine@gmail.com>
2021-01-16 14:26:49 +0100

    q 1

---
+++ js/script.js
@@ -0,0 +1,15 @@
+// question 1
+
+var outOfStock;
+var outOfStock = true;
+
+if (outOfStock === true) {
+    console.log("Out of stock");
+} else {
+    console.log("In stock");
+}
+
+console.log(outOfStock);
+
+//question 2
+
